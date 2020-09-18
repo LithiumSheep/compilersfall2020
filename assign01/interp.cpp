14561dc132e832bbc5ac055eb69ce246556cd86b
Jesse Li <jesse@sandboxx.us>
2020-09-18 14:19:37 -0400

    Half of the tests are working

--- assign01/interp.cpp
+++ assign01/interp.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <map>
 #include <cassert>
+#include <cmath>
 #include "util.h"
 #include "cpputil.h"
 #include "token.h"
@@ -59,18 +60,15 @@ long Interpreter::exec() {
 long Interpreter::eval(struct Node *expr) {
   // the number of children and the first child's tag will determine
   // how to evaluate the expression
-  struct Node *first = node_get_kid(expr, 0);
-  int num_kids = node_get_num_kids(expr);
-  int tag = node_get_tag(first);
 
-  if (num_kids == 1) {
-    // leaf expression (either an integer literal or identifier)
-    const char *lexeme = node_get_str(first);
-    if (tag == TOK_INTEGER_LITERAL) {
-      // convert lexeme to an integer value
-      return strtol(lexeme, nullptr, 10);
-    } else {
-      // look up value of variable
+  int num_kids = node_get_num_kids(expr);
+  int tag = node_get_tag(expr);
+
+  const char *lexeme = node_get_str(expr);
+  if (tag == TOK_INTEGER_LITERAL) {
+    return strtol(lexeme, nullptr, 10); 
+  } else if (tag == TOK_IDENTIFIER) {
+    // look up value of variable
       assert(tag == TOK_IDENTIFIER);
       std::map<std::string, long>::const_iterator i = m_vars.find(lexeme);
       if (i == m_vars.end()) {
@@ -78,41 +76,50 @@ long Interpreter::eval(struct Node *expr) {
         error_on_node(expr, errmsg.c_str());
       }
       return i->second;
-    }
   }
 
-  // left and right operands follow
-  struct Node *left = node_get_kid(expr, 1);
-  struct Node *right = node_get_kid(expr, 2);
+  if (num_kids == 1) {
+    struct Node *next = node_get_kid(expr, 0);
+    return eval(next);
+  } else {
+    struct Node *op = node_get_kid(expr, 1);  // operator is in the center
+    tag = node_get_tag(op);
+
+    // left and right operands follow
+    struct Node *left = node_get_kid(expr, 0);
+    struct Node *right = node_get_kid(expr, 2);
 
   // Do the evaluation
-  switch (tag) {
-  case TOK_PLUS:
-    return eval(left) + eval(right);
-  case TOK_MINUS:
-    return eval(left) - eval(right);
-  case TOK_TIMES:
-    return eval(left) * eval(right);
-  case TOK_DIVIDE:
-    return eval(left) / eval(right);
+    switch (tag) {
+    case TOK_PLUS:
+      return eval(left) + eval(right);
+    case TOK_MINUS:
+      return eval(left) - eval(right);
+    case TOK_TIMES:
+      return eval(left) * eval(right);
+    case TOK_DIVIDE:
+      return eval(left) / eval(right);
+    case TOK_POWER:
+      return pow(eval(left), eval(right));
 
-  case TOK_ASSIGN:
-    // in this case, the left operand is an identifier naming
-    // the variable
-    {
-      // get the variable name
-      std::string varname = node_get_str(left);
-      // evaluate the expression producing the value to be assigned
-      long rvalue = eval(right);
-      // store the value
-      m_vars[varname] = rvalue;
-      // result of the evaluation is the value assigned
-      return rvalue;
+    case TOK_ASSIGN:
+      // in this case, the left operand is an identifier naming
+      // the variable
+      {
+        // get the variable name
+        std::string varname = node_get_str(left);
+        // evaluate the expression producing the value to be assigned
+        long rvalue = eval(right);
+        // store the value
+        m_vars[varname] = rvalue;
+        // result of the evaluation is the value assigned
+        return rvalue;
+      }
+
+    default:
+      err_fatal("Unknown operator: %d\n", tag);
+      return -1L;
     }
-
-  default:
-    err_fatal("Unknown operator: %d\n", tag);
-    return -1L;
   }
 }
 
