ca43fbb0c64a971eda59027758ba3752b6be59fe
Jesse Li <jesse.elite@gmail.com>
2020-11-21 18:39:50 -0500

    Restore context

--- assign04/context.cpp
+++ assign04/context.cpp
@@ -364,24 +364,29 @@ public:
         Operand l_op = lhs->get_operand();
         Operand r_op = rhs->get_operand();
 
-        // ldi vr3, (vr1)
-        long lreg = next_vreg();
-        Operand ldest(OPERAND_VREG, lreg);
-        Operand lfrom(OPERAND_VREG_MEMREF, l_op.get_base_reg());
-        auto* lload = new Instruction(HINS_LOAD_INT, ldest, lfrom);
-        code->add_instruction(lload);
+        if (l_op.get_kind() != OPERAND_INT_LITERAL) {
+            // ldi vr3, (vr1)
+            long lreg = next_vreg();
+            Operand ldest(OPERAND_VREG, lreg);
+            Operand lfrom(OPERAND_VREG_MEMREF, l_op.get_base_reg());
+            auto* lload = new Instruction(HINS_LOAD_INT, ldest, lfrom);
+            code->add_instruction(lload);
+        }
 
-        // ldi vr4, (vr2)
-        long rreg = next_vreg();
-        Operand rdest(OPERAND_VREG, rreg);
-        Operand rfrom(OPERAND_VREG_MEMREF, r_op.get_base_reg());
-        auto* rload = new Instruction(HINS_LOAD_INT, rdest, rfrom);
-        code->add_instruction(rload);
+        if (r_op.get_kind() != OPERAND_INT_LITERAL) {
+            // ldi vr4, (vr2)
+            long rreg = next_vreg();
+            Operand rdest(OPERAND_VREG, rreg);
+            Operand rfrom(OPERAND_VREG_MEMREF, r_op.get_base_reg());
+            auto *rload = new Instruction(HINS_LOAD_INT, rdest, rfrom);
+            r_op = rdest;
+            code->add_instruction(rload);
+        }
 
         // addi vr5, vr3, vr4
         long result_reg = next_vreg();
         Operand adddest(OPERAND_VREG, result_reg);
-        auto* divins = new Instruction(HINS_INT_ADD, adddest, ldest, rdest);
+        auto* divins = new Instruction(HINS_INT_ADD, adddest, l_op, r_op);
         code->add_instruction(divins);
 
         ast->set_operand(adddest);
